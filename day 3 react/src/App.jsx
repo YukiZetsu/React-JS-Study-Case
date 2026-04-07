8232d02afe6707b6bc4d5f1185a322a9f45294d5
YukiZetsu <didik.pct001@gmail.com>
2026-04-07 16:14:28 +0700

    latihan day 3

---
+++ day 3 react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import FruitList from './Components/List/FruitList'
+import Services from './Components/Services'
+
+function App() {
+  return (
+    <>
+      {/* <FruitList /> */}
+      <Services />
+    </>
+  )
+}
+
+export default App
